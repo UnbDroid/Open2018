6f8d1e89dddfaac67e179812fce4d7f19e446c8f
Abdullah Zaiter <abdullah.zaiter@hotmail.com>
2018-10-07 12:16:45 -0300

    adicionando o codigo principal

---
+++ Codes/BeagleboneBlue/main/main.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+
+extern "C" {
+    #include <stdio.h>
+	#include <rc/mpu.h>
+	#include <rc/math.h>
+	#include <signal.h>
+	#include <rc/button.h>
+	#include <rc/encoder.h>
+	#include <rc/time.h>
+	#include <stdlib.h> // for atoi
+	#include <getopt.h>
+	#include <rc/motor.h>
+	#include <rc/pthread.h>
+	#include <rc/spi.h>
+	#include <rc/servo.h>
+	#include <rc/adc.h>
+}
+
+using namespace std;
+
+
+
+#define PI 3.14159265f
+
+int main () {
+	return 0;
+}
+
