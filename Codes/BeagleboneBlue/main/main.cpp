27796f834f58651df7f0b0ed615b47b8a000943d
Abdullah Zaiter <abdullah.zaiter@hotmail.com>
2018-10-13 19:04:33 -0300

    integracao c++ com spi com teste de compilacao no bbb

--- Codes/BeagleboneBlue/main/main.cpp
+++ Codes/BeagleboneBlue/main/main.cpp
@@ -25,7 +25,50 @@ using namespace std;
 
 #define PI 3.14159265f
 
-int main () {
+// change these for your platform
+// on BB this is equivilant to RC_BB_SPI1_SS1
+#define BUS		1
+#define SLAVE		0
+
+
+#define SLAVE_MODE	SPI_SLAVE_MODE_AUTO
+#define BUS_MODE	SPI_MODE_0
+#define SPI_SPEED	5000
+
+
+vector<char> SpiComm(int channel, vector<char> dado)
+{
+	char* test_str = reinterpret_cast<char*>(dado.data());
+	int bytes = 13;	// get number of bytes in test string
+	uint8_t buf[bytes];		// read buffer
+	int ret;			// return value
+	int batata = channel;
+	if(rc_spi_init_auto_slave(BUS, SLAVE, BUS_MODE, SPI_SPEED)){
+		vector<char> vec = {'F'};
+		return vec;
+	}
+
+	// attempt a string send/receive test
+	printf("Sending  %d bytes: %s\n", bytes, test_str);
+	ret = rc_spi_transfer(BUS, SLAVE, (uint8_t*)test_str, bytes, buf);
+	if(ret<0){
+		printf("send failed\n");
+		rc_spi_close(SLAVE);
+		vector<char> vec = {'F'};
+		return vec;
+	}
+	else printf("Received %d bytes: %s\n",ret, buf);
+
+	vector<char> vec(buf, buf+bytes);
+	rc_spi_close(BUS);
+	return vec;
+}
+
+int main () 
+{
+	vector<char> dados = {'2','3','4','5'};
+	vector<char> retorno = SpiComm(1,dados);
+	//cout << retorno<<endl;
 	return 0;
 }
 
