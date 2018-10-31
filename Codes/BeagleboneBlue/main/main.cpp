40fb26d0149c2c3fff5873fd9b1bd6f68ebe11c5
Abdullah Zaiter <abdullah.zaiter@hotmail.com>
2018-10-31 08:05:35 -0700

    main atualizada e mais organizada

--- Codes/BeagleboneBlue/main/main.cpp
+++ Codes/BeagleboneBlue/main/main.cpp
@@ -23,74 +23,64 @@ extern "C" {
 using namespace std;
 
 #define PI 3.14159265f
-[[gnu::unused]] static void __signal_handler( [[gnu::unused]] int dummy);
-
-/*-------------definicoes do SPI------------*/
 
 
-#define CANAL_CHASSIS 0
-#define CANAL_GARRA 1
+/*--------------definicoes do SPI---------------*/
+	#define CANAL_CHASSIS 0
+	#define CANAL_GARRA 1
 
-#define BARRAMENTO_CHASSIS RC_BB_SPI1_SS1
-#define BARRAMENTO_GARRA RC_BB_SPI1_SS2
+	#define BARRAMENTO_CHASSIS RC_BB_SPI1_SS1
+	#define BARRAMENTO_GARRA RC_BB_SPI1_SS2
 
-#define SLAVE_MODE	SPI_SLAVE_MODE_AUTO
-#define BUS_MODE	SPI_MODE_0
-#define SPI_SPEED	100000
-string SpiComm(int canal, char* dado, int tamanho_resposta);
+	#define SLAVE_MODE	SPI_SLAVE_MODE_AUTO
+	#define BUS_MODE	SPI_MODE_0
+	#define SPI_SPEED	100000
+	string SpiComm(int canal, char* dado, int tamanho_resposta);
+/*-----------definicoes dos sensores------------*/
+	#define QUANTIDADE_LDR 8
+	#define QUANTIDADE_COR 4
+	#define TAMANHO_RESPOSTA_CHASSIS 16
+	#define SENSORES_LDR 1
+	#define SENSORES_COR 2
+	vector<uint8_t> sensoresLDR(8);
+	vector<uint8_t> sensoresCOR(4);
 
+	void imprimeLeituras(int sensores);
+	void lerSensoresChassis();
+/*----------definicoes signal handling----------*/
 
-/*---------definicoes dos sensores----------*/
+	static int running;
+	[[gnu::unused]] static void __signal_handler( [[gnu::unused]] int dummy);
+/*------------definicoes motores----------------*/
 
-#define QUANTIDADE_LDR 8
-#define QUANTIDADE_COR 4
-#define TAMANHO_RESPOSTA_CHASSIS 16
-#define SENSORES_LDR 1
-#define SENSORES_COR 2
-vector<uint8_t> sensoresLDR(8);
-vector<uint8_t> sensoresCOR(4);
+	#define MOTOR_FRENTE 1
+	#define MOTOR_TRAS 2
+	#define MOTOR_DIREITA 3
+	#define MOTOR_ESQUERDA 4
+	#define TODOS_OS_MOTORES 0
 
-void imprimeLeituras(int sensores);
-void lerSensoresChassis();
+	#define DUTY_CYCLE_MAXIMO 0.725f//normaliza potencia
+	#define NORMALIZA_POTENCIA DUTY_CYCLE_MAXIMO/100
 
-/*---------definicoes signal handling----------*/
-static int running;
+	#define DIRECAO_LADO_Y 1
+	#define DIRECAO_SENSORES_X 2
 
+	#define QUANTIDADE_PULSOS_POR_REV 350
+	#define QUANTIDADE_PULSOS_PRECISAO 5
+	#define DIAMETRO_DA_RODA 11
+/*------------definicoes locomocao--------------*/
+	#define X_POS 1
+	#define X_NEG 2
+	#define Y_POS 3
+	#define Y_NEG 4
 
+	void andaMotores(int direcao, float vel);
+	int inicializa();
+	void andaDistancia(float dist, int eixo);
+	bool acabouDeAndar(long long int cont1, long long int cont2, float dist);
 
-/*------------definicoes motores---------------*/
-#define MOTOR_FRENTE 1
-#define MOTOR_TRAS 2
-#define MOTOR_DIREITA 3
-#define MOTOR_ESQUERDA 4
-#define TODOS_OS_MOTORES 0
-
-#define DUTY_CYCLE_MAXIMO 0.725f//normaliza potencia
-#define NORMALIZA_POTENCIA DUTY_CYCLE_MAXIMO/100
-
-#define DIRECAO_LADO_Y 1
-#define DIRECAO_SENSORES_X 2
-
-#define QUANTIDADE_PULSOS_POR_REV 350
-#define QUANTIDADE_PULSOS_PRECISAO 5
-#define DIAMETRO_DA_RODA 11
-
-/*------------definicoes locomocao-------------*/
-#define X_POS 1
-#define X_NEG 2
-#define Y_POS 3
-#define Y_NEG 4
-
-
-void andaMotores(int direcao, float vel);
-int inicializa();
-void andaDistancia(float dist, int eixo);
-bool acabouDeAndar(long long int cont1, long long int cont2, float dist);
-
-vector<float> orientacao(3);
-
-
-/*-----------definicoes mpu--------------------*/
+	vector<float> orientacao(3);
+/*------------definicoes mpu--------------------*/
 // bus for Robotics Cape and BeagleboneBlue is 2, interrupt pin is on gpio3.21
 // change these for your platform
 #define I2C_BUS 2
@@ -98,17 +88,10 @@ vector<float> orientacao(3);
 #define GPIO_INT_PIN_PIN  21
 static rc_mpu_data_t data;
 
-void __atualizaOrientacao()
-{
-	orientacao[0] = data.dmp_TaitBryan[TB_PITCH_X]*RAD_TO_DEG;
-	orientacao[1] = data.dmp_TaitBryan[TB_ROLL_Y]*RAD_TO_DEG;
-	orientacao[2] = data.dmp_TaitBryan[TB_YAW_Z]*RAD_TO_DEG;
-}
+
 
 int main () 
 {
-	//vector<char> dados = {'x','l'};
-	//char* dados = "s";
 	if (inicializa())
 	{
 		cout << "deu ruim na inicializacao"<<endl;
@@ -119,9 +102,10 @@ int main ()
 	string retorno = string(SpiComm(0,(char *)"s",16));
 	cout << retorno <<endl<<retorno.length()<<endl;
 	
-	lerSensoresChassis();
-	imprimeLeituras(SENSORES_LDR);
-	imprimeLeituras(SENSORES_COR);
+
+	//lerSensoresChassis();
+	//imprimeLeituras(SENSORES_LDR);
+	//imprimeLeituras(SENSORES_COR);
 
 
 	while(running)
@@ -133,6 +117,12 @@ int main ()
 	return 0;
 }
 
+void __atualizaOrientacao()
+{
+	orientacao[0] = data.dmp_TaitBryan[TB_PITCH_X]*RAD_TO_DEG;
+	orientacao[1] = data.dmp_TaitBryan[TB_ROLL_Y]*RAD_TO_DEG;
+	orientacao[2] = data.dmp_TaitBryan[TB_YAW_Z]*RAD_TO_DEG;
+}
 
 int inicializa()
 {
