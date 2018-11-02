e1624ac7b6e3035910ed7b35468e48b6b8bc7885
Abdullah Zaiter <abdullah.zaiter@hotmail.com>
2018-11-02 01:50:36 -0700

    script de envio melhorado, tem que testar com o bbb ligado

--- Codes/BeagleboneBlue/main/sendtobbb.sh
+++ Codes/BeagleboneBlue/main/sendtobbb.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-if [ "$1" != "" ]; then
-	sshpass -p temppwd scp -r ./*.h* debian@192.168.8.1:~/"$1"/ 
-    sshpass -p temppwd scp -r ./*.c* debian@192.168.8.1:~/"$1"/
+if [[ "$1" != "" ]]; then
+	if [[ "$2" != "" ]]; then
+		sshpass -p temppwd scp -r ./"$1" debian@192.168.8.1:~/"$2"/
+		sshpass -p temppwd scp -r ./"$1" debian@192.168.8.1:~/"$2"/
+	else
+		echo "pra qual pasta quer enviar seu arquivo"
+	fi
 else
-    sshpass -p temppwd scp -r ./*.c* debian@192.168.8.1:~/code/Open2018/Codes/BeagleboneBlue/main/
-    sshpass -p temppwd scp -r ./*.h* debian@192.168.8.1:~/code/Open2018/Codes/BeagleboneBlue/main/
+	sshpass -p temppwd scp -r ./m*.c* debian@192.168.8.1:~/code/
+	sshpass -p temppwd scp -r ./m*.h* debian@192.168.8.1:~/code/
 fi
-
