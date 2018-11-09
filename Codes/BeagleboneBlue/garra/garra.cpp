eb60bc2b8b6c5be0819817e6a83ec36ff05708e5
rafael-at97 <rafael.a.t97@gmail.com>
2018-11-09 09:29:12 -0200

    Codigo pra garra atualizado

--- Codes/BeagleboneBlue/garra/garra.cpp
+++ Codes/BeagleboneBlue/garra/garra.cpp
@@ -1,3 +1,5 @@
+#include<iostream>
+
 extern "C" {
     #include <stdio.h>
     #include <stdlib.h> // for atoi
@@ -21,19 +23,25 @@ using namespace std;
 #define PI 3.14159265f
 
 // Chip number in the beaglebone
-#define CHIP_1 1
+#define CHIP_1 3
 #define CHIP_2 3
 
 // Pin numbers in the respective chipset
-#define PIN_1 17
+#define PIN_1 20	
 #define PIN_2 17
+#define END_SWITCH 17
 
-#define PERIOD 2000000 // In microseconds (2000000) = 2 seconds per step
+/*
+#define PERIOD 500 // In microseconds (2000000) = 2 seconds per step
+*/
+
+int PERIOD;
 
 static int running = 1;
 
 // Deal with keyboard interrupt
 [[gnu::unused]] static void __signal_handler([[gnu::unused]] int dummy){
+	cout << "Keyboard Interrupt!" << endl;
 	running = 0;
 	return;
 }
@@ -54,8 +62,9 @@ bool setup_step_and_dir_pins(int chip1, int step, int chip2, int dir){
 }
 
 // Clean the step and dir pins used during the code
-void clean_all(int chip1, int step, int chip2, int dir){
+void clean_all(int chip1, int step, int end_switch, int chip2, int dir){
 	rc_gpio_cleanup(chip1, step);
+	rc_gpio_cleanup(chip1, end_switch);
 	rc_gpio_cleanup(chip2, dir);	
 }
 
@@ -105,47 +114,83 @@ bool send_pulses(int n_steps){
 		return 1; // Error
 	};
 	
-	rc.usleep(100); // 100us for safety
+	rc_usleep(100); // 100us for safety
 	
 	while(n_steps && running){
 		// Send a pulse based on time
 		if(step_up()){
 			return 1;
 		};
-		rc.usleep(PERIOD/2);
+		rc_usleep(PERIOD/2);
 		if(step_down()){
 			return 1;
 		};
-		rc.usleep(PERIOD/2);
+		rc_usleep(PERIOD/2);
+		
+		n_steps--;
 	};
 	
 	return 0;	// Everything alright
 }
 
+// Set the end-switch as INPUT
+bool set_switch(){
+	if(rc_gpio_init(CHIP_1, END_SWITCH, GPIOHANDLE_REQUEST_INPUT)){
+		// Erro, nao foi possivel inicializar a leitura
+		return 1;
+	};
+	
+	return 0;
+}
+
+void set_speed(int SPS){
+	PERIOD = 1000000.0/SPS;		//	(microseconds/second) / (steps/second) = (microseconds/steps)
+
+	if(PERIOD < 5){
+		PERIOD = 5;
+	};
+}
+
 int main(){
-	int i=0;
-
 	// Define pins in system as GPIO
-	system("config-pin p9_23 gpio");
-	system("config-pin p9_28 gpio");
+	//system("config-pin p9_23 gpio");
+	//system("config-pin p9_28 gpio");
 
-	// Inicializa os 2 pinos GPIO do chip 1 como output (pino 4 e 6)
+	int value = 0;
+
+	// Inicializa os 2 pinos GPIO do chip 1 como output (pino 3 e 6)
 	if(setup_step_and_dir_pins(CHIP_1, PIN_1, CHIP_2, PIN_2)){
 		// If got here, an error ocurred, clean and exit
-		clean_all(CHIP_1, PIN_1, CHIP_2, PIN_2);
+		clean_all(CHIP_1, PIN_1, END_SWITCH, CHIP_2, PIN_2);
 		return 1;
 	};
 
-	if(send_pulses(10)){
-		clean_all(CHIP_1, PIN_1, CHIP_2, PIN_2);
-		return 1;
-	};
-
-	if(send_pulses(-10)){
-		clean_all(CHIP_1, PIN_1, CHIP_2, PIN_2);
+/*
+	if(set_switch()){
 		return 1;
 	};	
 
-	clean_all(CHIP_1, PIN_1, CHIP_2, PIN_2);
+	while(running){
+		value = rc_gpio_get_value(CHIP_1, END_SWITCH);
+		if(value<0){
+			return 1;
+		};
+		cout << value << endl;
+	};
+*/
+
+	set_speed(200);
+
+	if(send_pulses(800)){
+		clean_all(CHIP_1, PIN_1, END_SWITCH, CHIP_2, PIN_2);
+		return 1;
+	};
+
+	if(send_pulses(-800)){
+		clean_all(CHIP_1, PIN_1, END_SWITCH, CHIP_2, PIN_2);
+		return 1;
+	};	
+
+	clean_all(CHIP_1, PIN_1, END_SWITCH, CHIP_2, PIN_2);
 	return 0;
 };
